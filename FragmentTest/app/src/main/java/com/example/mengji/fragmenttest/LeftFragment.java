2a5a08511da1440f630f49565020f70a42e7c90b
mengjikd <mengjikd@126.com>
2017-12-17 21:41:55 +0800

    4

---
+++ FragmentTest/app/src/main/java/com/example/mengji/fragmenttest/LeftFragment.java
@@ -0,0 +1,21 @@
+package com.example.mengji.fragmenttest;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by mengji on 2017/12/17.
+ */
+
+public class LeftFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.left_fragment,container,false);
+        return view;
+    }
+}
