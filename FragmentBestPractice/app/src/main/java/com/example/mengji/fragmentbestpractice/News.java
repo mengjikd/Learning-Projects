816d80c3c52c6108c985f87272501e451dc3bc8f
mengjikd <mengjikd@126.com>
2017-12-18 21:32:56 +0800

    5

---
+++ FragmentBestPractice/app/src/main/java/com/example/mengji/fragmentbestpractice/News.java
@@ -0,0 +1,22 @@
+package com.example.mengji.fragmentbestpractice;
+
+/**
+ * Created by mengji on 2017/12/18.
+ */
+
+public class News {
+    private String title;
+    private String content;
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title ){
+        this.title = title;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content = content;
+    }
+}
