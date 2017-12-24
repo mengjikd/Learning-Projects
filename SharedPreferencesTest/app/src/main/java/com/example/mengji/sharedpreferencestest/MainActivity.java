c075f432aea1d079ff9f12dd0dfa7cc6310156c8
mengjikd <mengjikd@126.com>
2017-12-24 21:57:56 +0800

    9

--- SharedPreferencesTest/app/src/main/java/com/example/mengji/sharedpreferencestest/MainActivity.java
+++ SharedPreferencesTest/app/src/main/java/com/example/mengji/sharedpreferencestest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mengji.sharedpreferencestest;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -21,6 +22,19 @@ public class MainActivity extends AppCompatActivity {
                 editor.putInt("age",28);
                 editor.putBoolean("married",false);
                 editor.apply();
+            }
+        });
+        Button restoreData = (Button) findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age = pref.getInt("age",0);
+                boolean married = pref.getBoolean("married",false);
+                Log.d("MainActivity","name is " + name);
+                Log.d("MainActivity","ages is " + age);
+                Log.d("MainActivity","married is " + married);
             }
         });
     }
