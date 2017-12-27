9adec87cb537a57cb89d3d3c58512e1eb7838ba4
mengjikd <mengjikd@126.com>
2017-12-27 21:50:40 +0800

    10

--- DatabaseTest/app/src/main/java/com/example/mengji/databasetest/MainActivity.java
+++ DatabaseTest/app/src/main/java/com/example/mengji/databasetest/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.mengji.databasetest;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -15,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelper = new MyDatabaseHelper(this, "BookStore.db",null,2);
-        Button creatDatabase = (Button) findViewById(R.id.create_database);
+        final Button creatDatabase = (Button) findViewById(R.id.create_database);
         creatDatabase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,6 +43,49 @@ public class MainActivity extends AppCompatActivity {
                 values.put("pages",510);
                 values.put("price",19.95);
                 db.insert("Book",null,values);//插入第二条数据
+            }
+        });
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues values = new ContentValues();
+                values.put("price",10.99);
+                db.update("Book",values,"name = ?",new String[] {
+                        "The Da Vinci Code"
+                });
+            }
+        });
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.delete("Book","pages > ?",new String[] { "500" });
+            }
+        });
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                //查询Book表中所有的数据
+                Cursor cursor = db.query("Book",null,null,null,null,null,null);
+                if (cursor.moveToFirst()) {
+                    do {
+                        //遍历Cursor对象，取出数据并打印
+                        String name = cursor.getString(cursor.getColumnIndex("name"));
+                        String author = cursor.getString(cursor.getColumnIndex("author"));
+                        int pages = cursor.getInt(cursor.getColumnIndex("pages"));
+                        double price = cursor.getDouble(cursor.getColumnIndex("price"));
+                        Log.d("MainActivity","book name is" + name);
+                        Log.d("MainActivity","book author is" + author);
+                        Log.d("MainActivity","book pages is" + pages);
+                        Log.d("MainActivity","book price is" + price);
+                    }while (cursor.moveToNext());
+                }
+                cursor.close();
             }
         });
     }
