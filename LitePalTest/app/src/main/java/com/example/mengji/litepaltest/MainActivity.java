97bcc003a81e1387cb48460d04b75098de1364f1
mengjikd <mengjikd@126.com>
2018-01-02 21:07:14 +0800

    {feat}|> updata

--- LitePalTest/app/src/main/java/com/example/mengji/litepaltest/MainActivity.java
+++ LitePalTest/app/src/main/java/com/example/mengji/litepaltest/MainActivity.java
@@ -2,10 +2,14 @@ package com.example.mengji.litepaltest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import org.litepal.LitePal;
+import org.litepal.crud.DataSupport;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +37,37 @@ public class MainActivity extends AppCompatActivity {
                 book.save();
             }
         });
+        Button updateData = (Button) findViewById(R.id.update_data);
+        updateData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Book book = new Book();
+                book.setPrice(14.95);
+                book.setPress("Anchor");
+                book.updateAll("name = ? and author = ?","The Lost Symbol","Dan Brown");
+            }
+        });
+        Button deleteButton = (Button) findViewById(R.id.delete_data);
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DataSupport.deleteAll(Book.class, "price < ?","15");
+            }
+        });
+        Button queryButton = (Button) findViewById(R.id.query_data);
+        queryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                List<Book> books = DataSupport.findAll(Book.class);
+                for (Book book: books){
+                    Log.d("MainActivity","book name is" + book.getName());
+                    Log.d("MainActivity","book author is" + book.getAuthor());
+                    Log.d("MainActivity","book pages is" + book.getPages());
+                    Log.d("MainActivity","book price is" + book.getPrice());
+                    Log.d("MainActivity","book press is" + book.getPress());
+                }
+            }
+        });
     }
 }
+
