4c73f7a67b792fb7127e96df95940d620359f3aa
mengjikd <mengjikd@126.com>
2018-01-05 21:42:56 +0800

    {feat}|> Database

--- DatabaseTest/app/src/main/java/com/example/mengji/databasetest/MyDatabaseHelper.java
+++ DatabaseTest/app/src/main/java/com/example/mengji/databasetest/MyDatabaseHelper.java
@@ -3,7 +3,6 @@ package com.example.mengji.databasetest;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by mengji on 2017/12/23.
@@ -31,7 +30,7 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext, "Create succeeded", Toast.LENGTH_SHORT).show();
+
     }
 
     @Override
