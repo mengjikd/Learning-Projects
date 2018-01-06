3686ab0f9b6bfb3b030802a16fa5346f1271b73a
mengjikd <mengjikd@126.com>
2018-01-06 21:10:09 +0800

    {feat}|> Network programming

---
+++ NetworkTest/app/src/main/java/com/example/mengji/networktest/App.java
@@ -0,0 +1,29 @@
+package com.example.mengji.networktest;
+
+/**
+ * Created by mengji on 2018/1/6.
+ */
+
+public class App {
+    private String id;
+    private String name;
+    private String version;
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getVersion() {
+        return version;
+    }
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
