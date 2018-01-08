290d6c2e0ecb839915215ca31ba65b46b14211dd
mengjikd <mengjikd@126.com>
2018-01-08 20:43:46 +0800

    {feat}|> Service projects

--- ServiceTest/app/src/main/java/com/example/mengji/servicetest/MainActivity.java
+++ ServiceTest/app/src/main/java/com/example/mengji/servicetest/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.mengji.servicetest;
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private MyService.DownloadBinder downloadBinder;
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +32,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button stopService = (Button) findViewById(R.id.stop_service);
         startService.setOnClickListener(this);
         stopService.setOnClickListener(this);
+        Button bindService = (Button) findViewById(R.id.bind_service);
+        Button unbinService = (Button) findViewById(R.id.unbind_service);
+        bindService.setOnClickListener(this);
+        unbinService.setOnClickListener(this);
     }
 
     @Override
@@ -28,6 +48,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this, MyService.class);
                 startService(stopIntent);//停止服务
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);//绑定服务
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);//解绑服务
                 break;
             default:
                 break;
